ebe4c86d0e98b459add4a9e3f80044e25dad40ae
gretaleege <greta.k.leege@vanderbilt.edu>
2025-02-11 17:29:21 +0100

    Pylint on exceptionManager

--- UC3MMoney1/TransactionManagementException.py
+++ UC3MMoney1/TransactionManagementException.py
@@ -1,13 +1,37 @@
+"""
+Transaction Management Exception
+
+This module defines the TransactionManagementException class, that
+handles errors in the TransactionManagement class
+"""
 class TransactionManagementException(Exception):
+    """
+    Exception class for TransactionManagementException
+
+    parameters:
+        Exception: message(string)-- the error message
+    """
     def __init__(self, message):
+        """
+        Initialize the exception with a message
+        :param message: description of the exception
+        """
         self.__message = message
         super().__init__(self.message)
 
     @property
     def message(self):
+        """
+        Gets the exception message
+
+        :return: the error message
+        """
         return self.__message
 
     @message.setter
     def message(self,value):
+        """
+        Sets the exception message
+        :param value: the new error message
+        """
         self.__message = value
-
