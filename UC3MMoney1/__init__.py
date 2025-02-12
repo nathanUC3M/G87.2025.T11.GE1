f4203bf5f22a8dd395853d7357458444257ba5da
gretaleege <greta.k.leege@vanderbilt.edu>
2025-02-12 14:28:53 +0100

    Cleaned up

--- UC3MMoney1/__init__.py
+++
@@ -1,7 +0,0 @@
-from UC3MMoney1.TransactionRequest import TransactionRequest
-from UC3MMoney1.TransactionManager import TransactionManager
-from UC3MMoney1.TransactionManagementException import TransactionManagementException
-
-
-print(TransactionManager.validate_iban("GB82WEST12345698765432"))  # true
-print(TransactionManager.validate_iban("FR7630006000011234567890189")) #false
